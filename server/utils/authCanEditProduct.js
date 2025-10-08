7ab92bd45f4ba94cc8a5edc79451923f576e64da
Peter <lakehub07@gmail.com>
2025-10-08 15:52:40 +0100

    yy

--- server/utils/authCanEditProduct.js
+++ server/utils/authCanEditProduct.js
@@ -1,8 +1,12 @@
 // check if current auth can edit a product
 const authCanEditProduct = (req, product) => {
+  console.log("product:", product); // Debugging log
+  console.log("req.user:", req.user); // Debugging log
+
   if (product && req.user) {
     if (product.seller.toString() !== req.user.id) return false;
   }
+  return true;
 };
 
 module.exports = authCanEditProduct;
