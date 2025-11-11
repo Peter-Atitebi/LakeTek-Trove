4f2707502cab1cbd533c0c280ab481df06dc9139
Peter <lakehub07@gmail.com>
2025-11-11 11:31:35 +0100

    `feat: Add store ID validation and authentication checks in product creation and store template pages`

--- server/utils/authCanEditProduct.js
+++ server/utils/authCanEditProduct.js
@@ -1,4 +1,6 @@
 // check if current auth can edit a product
+const jwt = require("jsonwebtoken");
+
 const authCanEditProduct = (req, product) => {
   console.log("product:", product); // Debugging log
   console.log("req.user:", req.user); // Debugging log
