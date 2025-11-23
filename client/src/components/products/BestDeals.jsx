169cb68c9bf9fd121943afe008c7c0ef0a4dbdc8
Peter <lakehub07@gmail.com>
2025-11-23 20:24:35 +0100

    `Added GallerySlider component to BestDeals component`

--- client/src/components/products/BestDeals.jsx
+++ client/src/components/products/BestDeals.jsx
@@ -1,11 +1,12 @@
 // src/components/products/BestDeals.jsx
-import { div } from "framer-motion/client";
 import React from "react";
+import GallerySlider from "./GallerySlider";
 
 const BestDeals = () => {
   return (
     <div>
       <h2 className="text-3xl font-bold mb-4">Best Deals</h2>
+      <GallerySlider />
     </div>
   );
 };
