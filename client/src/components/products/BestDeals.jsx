82bb0426925de023e3802c7d70d9f833e8848bb7
Peter <lakehub07@gmail.com>
2025-11-23 20:21:31 +0100

    ,m

---
+++ client/src/components/products/BestDeals.jsx
@@ -0,0 +1,13 @@
+// src/components/products/BestDeals.jsx
+import { div } from "framer-motion/client";
+import React from "react";
+
+const BestDeals = () => {
+  return (
+    <div>
+      <h2 className="text-3xl font-bold mb-4">Best Deals</h2>
+    </div>
+  );
+};
+
+export default BestDeals;
--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,14 +1,20 @@
 //src/pages/Home.jsx
-
 import AppLayout from "../components/AppLayout";
 import AppFooter from "../components/footer/AppFooter";
 import HomeFeed from "../components/products/HomeFeed";
+import BestDeals from "../components/products/BestDeals";
 
 const Home = () => {
   return (
     <>
       <AppLayout>
+        {/* Best Deals */}
+        <BestDeals />
+
+        {/* Home Feed */}
         <HomeFeed />
+
+        {/* Recently Viewed */}
       </AppLayout>
 
       {/* Footer */}
